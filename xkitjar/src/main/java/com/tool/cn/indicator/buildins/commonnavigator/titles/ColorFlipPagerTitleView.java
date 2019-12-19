b9f8ce0e9f20843d5baad8c6782a2cbbe2c075e8
zhangi789 <www.392032143@qq.com>
2019-12-19 14:17:02 +0800

    添加tablayout指示器

---
+++ xkitjar/src/main/java/com/tool/cn/indicator/buildins/commonnavigator/titles/ColorFlipPagerTitleView.java
@@ -0,0 +1,51 @@
+package com.tool.cn.indicator.buildins.commonnavigator.titles;
+
+import android.content.Context;
+
+/**
+ * author 张海洋
+ * create on   2019/12/19 12:18
+ * description   TODO
+ */
+
+public class ColorFlipPagerTitleView extends SimplePagerTitleView {
+    private float mChangePercent = 0.5f;
+
+    public ColorFlipPagerTitleView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onLeave(int index, int totalCount, float leavePercent, boolean leftToRight) {
+        if (leavePercent >= mChangePercent) {
+            setTextColor(mNormalColor);
+        } else {
+            setTextColor(mSelectedColor);
+        }
+    }
+
+    @Override
+    public void onEnter(int index, int totalCount, float enterPercent, boolean leftToRight) {
+        if (enterPercent >= mChangePercent) {
+            setTextColor(mSelectedColor);
+        } else {
+            setTextColor(mNormalColor);
+        }
+    }
+
+    @Override
+    public void onSelected(int index, int totalCount) {
+    }
+
+    @Override
+    public void onDeselected(int index, int totalCount) {
+    }
+
+    public float getChangePercent() {
+        return mChangePercent;
+    }
+
+    public void setChangePercent(float changePercent) {
+        mChangePercent = changePercent;
+    }
+}
